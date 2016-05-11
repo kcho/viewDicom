077ca2871e8809e4186f4eeafea140898908b8e0
kangik <kangik@neuroimage.snu.ac.kr>
2016-05-11 14:46:33 +0900

    one dicom reading option is added as a default

--- viewDicom.py
+++ viewDicom.py
@@ -1,5 +1,8 @@
+#!/ccnc_bin/venv/bin/python
+
 from __future__ import division
 import os
+import sys
 import matplotlib.pyplot as plt
 import dicom
 import numpy as np
@@ -7,8 +10,6 @@ import re
 import argparse
 import textwrap
 from progressbar import AnimatedMarker,ProgressBar,Percentage,Bar
-
-
 
 
 def filter_modality(modality, dicomList):
@@ -79,11 +80,8 @@ def returnDir(rootDir):
                     break
     return dirList 
 
-def viewDicom(dicomDir, sliceNum, outdir):
-    #name = re.sub(dicomDir, '\/', '_')
+def viewDicom(dicomDir, sliceNum, outdir, verbose=False):
     name = getName(dicomDir)
-    print 'haha'
-
 
     dicomFiles = [os.path.join(dicomDir,x) for x in os.listdir(dicomDir) if x.endswith('dcm') or \
                                                      x.endswith('IMA') or \
@@ -99,6 +97,7 @@ def viewDicom(dicomDir, sliceNum, outdir):
     try:
         # RefDs = dicom.read_file(dicomFiles[0])
         RefDs = dicom.read_file(dicomFiles[sliceNum])
+        dataArray = RefDs.pixel_array
         # Load dimensions based on the number of rows, columns, and slices (along the Z axis)
         ConstPixelDims = (int(RefDs.Rows), 
                           int(RefDs.Columns), 
@@ -113,23 +112,25 @@ def viewDicom(dicomDir, sliceNum, outdir):
         ArrayDicom = np.zeros(ConstPixelDims, dtype=RefDs.pixel_array.dtype)
 
     except:
-        RefDs = dicom.read_file(dicomFiles[0], force=True)
-        #rawData = RefDs.values()[0][3]
-        try:
-            rawData = RefDs.values()[0].repval
-        except:
-            rawData = RefDs.values()[0][3][-896*896*2:]
+        dataArray = forcedRead(dicomFiles[sliceNum])
+        ArrayDicom = np.zeros((dataArray.shape[-2],
+                               dataArray.shape[-1],
+                               len(dicomFiles)), dtype=float)
 
-        #num = rawData.index('A1/PFP/FS') + 18
-        imageMatrixList = map(ord, rawData)
-        print len(imageMatrixList)
-        data1 = np.array(imageMatrixList[0::2]).reshape((896,896))
-        data2 = np.array(imageMatrixList[1::2]).reshape((896,896))
-        ArrayDicom = np.zeros((896,896,65), dtype=float)
+
+    fig = plt.figure('Dicom viewer', dpi=300)
+    plt.axes().set_aspect('equal', 'datalim')
+    plt.set_cmap(plt.gray())
+
+    if verbose:
+        pass
+    else:
+        plt.pcolormesh(np.flipud(dataArray))
+        plt.show()
+        sys.exit('Finished')
 
 
     # loop through all the DICOM files
-
     num=0
     pbar=ProgressBar().start()
     for filenameDCM in dicomFiles:
@@ -141,27 +142,16 @@ def viewDicom(dicomDir, sliceNum, outdir):
             # store the raw image data
             ArrayDicom[:, :, dicomFiles.index(filenameDCM)] = ds.pixel_array  
         except:
-            ds = dicom.read_file(filenameDCM, force=True)
-            try:
-                rawData = ds.values()[0].repval
-            except:
-                rawData = RefDs.values()[0][3][-896*896*2:]
-            imageMatrixList = map(ord, rawData)
-            data1 = np.array(imageMatrixList[0::2]).reshape((896,896))
-            data2 = np.array(imageMatrixList[1::2]).reshape((896,896))
-            ArrayDicom[:, :, dicomFiles.index(filenameDCM)] = data1  
+            ArrayDicom[:, :, dicomFiles.index(filenameDCM)] = forcedRead(filenameDCM)
     pbar.finish()
 
 
-    fig = plt.figure('Dicom viewer', dpi=300)
-    plt.axes().set_aspect('equal', 'datalim')
-    plt.set_cmap(plt.gray())
 
     #plt.pcolormesh(RefDs.pixel_array)
     try:
         if 'MOSAIC' in ds.ImageType:
             # DTI
-            mosaic_data = np.flipud(ArrayDicom[:, :, 10])
+            mosaic_data = np.flipud(ArrayDicom[:, :, sliceNum])
             slices = blockshaped(mosaic_data, 128, 128)#$[:,:,:]
             stacked = np.hstack([
                 np.flipud(slices[38,:,:]), 
@@ -181,8 +171,9 @@ def viewDicom(dicomDir, sliceNum, outdir):
     #cfm = plt.get_current_fig_manager('Dicom viewer')
     #cfm.window.raise_Tue 10 May 2016 10:27:35()
 
-    print os.path.join(outdir, name+'.png')
-    plt.savefig(os.path.join(outdir, name+'.png'))
+    #print os.path.join(outdir, name+'.png')
+    #plt.savefig(os.path.join(outdir, name+'.png'))
+    plt.show()
 
 
 
@@ -198,6 +189,41 @@ def blockshaped(arr, nrows, ncols):
     return (arr.reshape(h//nrows, nrows, -1, ncols)
                .swapaxes(1,2)
                .reshape(-1, nrows, ncols))
+
+def forcedRead(dicomFile):
+    '''
+    When normal pydicom does not work,
+    "force" option is given True.
+
+    Returns numpy array
+    '''
+    ds = dicom.read_file(dicomFile, force=True)
+
+    rawInfo = ds.values()[0]
+
+    if 'repval' in dir(rawInfo):
+        rawData = rawInfo.repval
+    elif 'value' in dir(rawInfo):
+        rawData = rawInfo.value
+    else:
+        rawData = RefDs.values()[0][3]
+
+    matrixLenDict = {'dti':(2, 896,896), 
+                    'dki':(2, 896,896),
+                    'T2':(320,288),
+                    'rest':(128,128),
+                    'T1':(256,256)}
+    for modality, matShape in matrixLenDict.iteritems():
+        if modality in dicomFile:
+            matrixLen = reduce(lambda x, y: x*y, matShape) 
+            data = map(ord,rawData[-matrixLen:])
+            data_array = np.array(data).reshape(matShape)
+
+    return data_array
+
+
+    
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -220,11 +246,16 @@ if __name__ == '__main__':
         help='png output location',
         default=os.getcwd(),
         )
+    parser.add_argument(
+        '-v', '--verbose',
+        help='Calculate whole image array',
+        action='store_true',
+        default=False
+        )
 
     args = parser.parse_args()
 
-    #viewDicom(args.dicomDir, int(args.sliceNumber), args.out)
-    prac()
+    viewDicom(args.dicomDir, int(args.sliceNumber), args.out, args.verbose)
 
 
 
