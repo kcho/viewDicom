8179e146272b234a613b24e75711f14136f4d767
kangik <kangik@neuroimage.snu.ac.kr>
2016-05-11 11:04:10 +0900

    force reading options added as exception, dirty version

--- viewDicom.py
+++ viewDicom.py
@@ -1,12 +1,55 @@
 from __future__ import division
 import os
-from matplotlib import pyplot, cm
+import matplotlib.pyplot as plt
 import dicom
-import numpy
+import numpy as np
 import re
 import argparse
 import textwrap
 from progressbar import AnimatedMarker,ProgressBar,Percentage,Bar
+
+
+
+
+def filter_modality(modality, dicomList):
+    newDicomList = []
+    for dicomName in dicomList:
+        if re.search(modality, dicomName):
+            newDicomList.append(dicomName)
+    return newDicomList
+
+
+def getName(dicomDir):
+    try:
+        searched = re.search('([A-Z]{3}\d{2,3}_[A-Z]{2,3})/(baseline|followup)', dicomDir, re.IGNORECASE)
+        name  = '_'.join([searched.group(1), searched.group(2)])
+    except:
+        name = re.sub('\/','_',dicomDir)
+    return name
+
+def prac():
+    #dataLocation = '/Volumes/promise/CCNC_MRI_3T/NOR'
+    dataLocation = os.getcwd()
+    dirList = returnDir(dataLocation)
+    dtiList = filter_modality('DTI', dirList)
+    for dicomDir in dtiList:
+        viewDicom(dicomDir, 0, '/Volumes/promise/cerebellum')
+
+
+def noMixDicom(dicomList):
+    instanceNumList = []
+    for i in dicomList:
+        ds = dicom.read_file(i)
+        instanceNum = ds.InstanceNumber
+        if instanceNum in instanceNumList:
+            print 'Not all dicoms are from one modality !'
+            print 'Please check'
+            return False
+            break
+        else:
+            instanceNumList.append(instanceNum)
+    print 'All from one modality'
+    return True
 
 
 def returnDir(rootDir):
@@ -36,74 +79,54 @@ def returnDir(rootDir):
                     break
     return dirList 
 
-
-def filter_modality(modality, dicomList):
-    newDicomList = []
-    for dicomName in dicomList:
-        if re.search(modality, dicomName):
-            newDicomList.append(dicomName)
-    return newDicomList
-
-
-def getName(dicomDir):
-    try:
-        name = re.search('([A-Z]{3}\d{2,3}_[A-Z]{2,3}', dicomName).group(1)
-    except:
-        name = re.sub('\/','_',dicomName)
-    return name
-
-def prac():
-    dataLocation = '/Volumes/promise/CCNC_MRI_3T/NOR'
-    dirList = returnDir(dataLocation)
-    dtiList = filter_modality('DTI', dirList)
-    for dicomDir in dtiList:
-        try:
-            viewDicom(dicomDir, 0, '/Volumes/promise/cerebellum')
-        except:
-            print dicomDir, 'does not work'
-
-
-def noMixDicom(dicomList):
-    instanceNumList = []
-    for i in dicomList:
-        ds = dicom.read_file(i)
-        instanceNum = ds.InstanceNumber
-        if instanceNum in instanceNumList:
-            return False
-            break
-        else:
-            instanceNumList.append(instanceNum)
-    return True
-
-
 def viewDicom(dicomDir, sliceNum, outdir):
     #name = re.sub(dicomDir, '\/', '_')
     name = getName(dicomDir)
+    print 'haha'
 
 
     dicomFiles = [os.path.join(dicomDir,x) for x in os.listdir(dicomDir) if x.endswith('dcm') or \
                                                      x.endswith('IMA') or \
                                                      x.endswith('DCM')]
 
-    if noMixDicom(dicomFiles):
-        pass
-    else:
-        os.error('There are mixture of dicoms')
+    #if noMixDicom(dicomFiles):
+        #pass
+    #else:
+        #os.error('There are mixture of dicoms')
 
 
     # Get ref file
-    # RefDs = dicom.read_file(dicomFiles[0])
-    RefDs = dicom.read_file(dicomFiles[sliceNum])
+    try:
+        # RefDs = dicom.read_file(dicomFiles[0])
+        RefDs = dicom.read_file(dicomFiles[sliceNum])
+        # Load dimensions based on the number of rows, columns, and slices (along the Z axis)
+        ConstPixelDims = (int(RefDs.Rows), 
+                          int(RefDs.Columns), 
+                          len(dicomFiles))
 
-    # Load dimensions based on the number of rows, columns, and slices (along the Z axis)
-    ConstPixelDims = (int(RefDs.Rows), int(RefDs.Columns), len(dicomFiles))
+        # Load spacing values (in mm)
+        ConstPixelSpacing = (float(RefDs.PixelSpacing[0]), 
+                             float(RefDs.PixelSpacing[1]), 
+                             float(RefDs.SliceThickness))
 
-    # Load spacing values (in mm)
-    ConstPixelSpacing = (float(RefDs.PixelSpacing[0]), float(RefDs.PixelSpacing[1]), float(RefDs.SliceThickness))
+        # The array is sized based on 'ConstPixelDims'
+        ArrayDicom = np.zeros(ConstPixelDims, dtype=RefDs.pixel_array.dtype)
 
+    except:
+        RefDs = dicom.read_file(dicomFiles[0], force=True)
+        #rawData = RefDs.values()[0][3]
+        try:
+            rawData = RefDs.values()[0].repval
+        except:
+            rawData = RefDs.values()[0][3][-896*896*2:]
 
-    # The array is sized based on 'ConstPixelDims'
-    ArrayDicom = numpy.zeros(ConstPixelDims, dtype=RefDs.pixel_array.dtype)
+        #num = rawData.index('A1/PFP/FS') + 18
+        imageMatrixList = map(ord, rawData)
+        print len(imageMatrixList)
+        data1 = np.array(imageMatrixList[0::2]).reshape((896,896))
+        data2 = np.array(imageMatrixList[1::2]).reshape((896,896))
+        ArrayDicom = np.zeros((896,896,65), dtype=float)
+
 
     # loop through all the DICOM files
 
@@ -113,42 +136,53 @@ def viewDicom(dicomDir, sliceNum, outdir):
         pbar.update(num)
         num+=100 / len(dicomFiles)
         # read the file
-        ds = dicom.read_file(filenameDCM)
-        # store the raw image data
-        #ArrayDicom[:, :, dicomFiles.index(filenameDCM)] = ds.pixel_array  
-        ArrayDicom[:, :, filenameDCM] = ds.pixel_array  
+        try:
+            ds = dicom.read_file(filenameDCM)
+            # store the raw image data
+            ArrayDicom[:, :, dicomFiles.index(filenameDCM)] = ds.pixel_array  
+        except:
+            ds = dicom.read_file(filenameDCM, force=True)
+            try:
+                rawData = ds.values()[0].repval
+            except:
+                rawData = RefDs.values()[0][3][-896*896*2:]
+            imageMatrixList = map(ord, rawData)
+            data1 = np.array(imageMatrixList[0::2]).reshape((896,896))
+            data2 = np.array(imageMatrixList[1::2]).reshape((896,896))
+            ArrayDicom[:, :, dicomFiles.index(filenameDCM)] = data1  
     pbar.finish()
 
 
-    fig = pyplot.figure('Dicom viewer', dpi=300)
-    pyplot.axes().set_aspect('equal', 'datalim')
-    pyplot.set_cmap(pyplot.gray())
+    fig = plt.figure('Dicom viewer', dpi=300)
+    plt.axes().set_aspect('equal', 'datalim')
+    plt.set_cmap(plt.gray())
 
-    #pyplot.pcolormesh(RefDs.pixel_array)
-    if 'MOSAIC' in ds.ImageType:
-        # DTI
-        mosaic_data = ArrayDicom[:, :, 10]
-        slices = blockshaped(mosaic_data, 128, 128)#$[:,:,:]
-        print slices.shape, sliceNum
-        stacked = numpy.hstack([
-            numpy.flipud(slices[38,:,:]), 
-            numpy.flipud(slices[39,:,:]),
-            numpy.flipud(slices[40,:,:])
-            ])
-        #pyplot.pcolormesh(stacked)
-        pyplot.pcolormesh(mosaic_data)
+    #plt.pcolormesh(RefDs.pixel_array)
+    try:
+        if 'MOSAIC' in ds.ImageType:
+            # DTI
+            mosaic_data = np.flipud(ArrayDicom[:, :, 10])
+            slices = blockshaped(mosaic_data, 128, 128)#$[:,:,:]
+            stacked = np.hstack([
+                np.flipud(slices[38,:,:]), 
+                np.flipud(slices[39,:,:]),
+                np.flipud(slices[40,:,:])
+                ])
+            #plt.pcolormesh(stacked)
+            plt.pcolormesh(mosaic_data)
 
-    else:
-        pyplot.pcolormesh(numpy.flipud(ArrayDicom[:, :, sliceNum]))
-        #pyplot.pcolormesh(numpy.flipud(ArrayDicom[:, -sliceNum, :]))
-    #pyplot.imshow(ds.pixel_array)
+        else:
+            plt.pcolormesh(np.flipud(ArrayDicom[:, :, sliceNum]))
+            #plt.pcolormesh(np.flipud(ArrayDicom[:, -sliceNum, :]))
+        #plt.imshow(ds.pixel_array)
+    except:
+        plt.pcolormesh(np.flipud(ArrayDicom[:,:,sliceNum]))
 
-    #cfm = pyplot.get_current_fig_manager('Dicom viewer')
+    #cfm = plt.get_current_fig_manager('Dicom viewer')
     #cfm.window.raise_Tue 10 May 2016 10:27:35()
 
-    print 'ha'
     print os.path.join(outdir, name+'.png')
-    pyplot.savefig(os.path.join(outdir, name+'.png'))
+    plt.savefig(os.path.join(outdir, name+'.png'))
 
 
 
